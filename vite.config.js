eb70f6fb6cb01f8c92299d5b20478969d082dc0c
kodak91 <realsmell@gmail.com>
2026-05-11 23:49:34 +0900

    fix: 서비스 워커 즉시 활성화 (skipWaiting + clientsClaim)

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,9 @@ export default defineConfig({
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        skipWaiting: true,
+        clientsClaim: true
       }
     })
   ]
